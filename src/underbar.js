cee48b65cb5a1e6b90b30b65cb3a24c318537759
Marissa <marissa.shieh@gmail.com>
2019-01-21 16:23:42 -0500

    Trying filter the way they want me to

--- src/underbar.js
+++ src/underbar.js
@@ -100,16 +100,8 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
-    
-     var resultsArr = [];
-    
-     _.each( collection, function(item) {
-      if ( !test(item) ){
-         resultsArr.push(item);        
-      }
-     });
 
-     return resultsArr;
+    return _.filter(collection,test);
 
   };
 
