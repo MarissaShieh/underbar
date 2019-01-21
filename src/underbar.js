0bb15c6386fd07b072e3445b1e2d031b4292a1e4
Marissa <marissa.shieh@gmail.com>
2019-01-21 16:29:25 -0500

    Reject function

--- src/underbar.js
+++ src/underbar.js
@@ -101,7 +101,7 @@
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
 
-    return _.filter(collection, function(){test===false});
+    return _.filter(collection, test===false);
 
   };
 
