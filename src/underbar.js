6e7f3dd46c85add0cef2c4ca1fa8a09c8269e896
Marissa <marissa.shieh@gmail.com>
2019-01-21 21:58:18 -0500

    Cleaned up syntax in Reduce

--- src/underbar.js
+++ src/underbar.js
@@ -192,15 +192,15 @@
 
     _.each(collection, function(item){
       if (accumulator === undefined){
-        accumulator = item
-      } if (iterator === undefined){
-        accumulator = item
+        accumulator = item;
+      } else if (iterator === undefined){
+        accumulator = item;
       }
 
       else {
         accumulator = iterator(accumulator, item);
       }
-      });
+    });
 
     return accumulator;
 
